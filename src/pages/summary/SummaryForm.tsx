9922262cfff34cf57c1acff7f96dd4c791aac0cc
Aaron Janes <aaron.janes@gmail.com>
2021-08-24 22:04:19 -0600

    2nd set of tests on disabled state

--- src/pages/summary/SummaryForm.tsx
+++ src/pages/summary/SummaryForm.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import {Container, Button, Form} from "react-bootstrap"
+import React, {useState} from 'react';
+import {Container, Col, Button, Form} from "react-bootstrap"
 
 function SummaryForm() {
+  const [disabled, setDissabled] = useState(true)
+
+  const CheckBoxLabel = (
+    <span>
+
+    I agree to <span style={{color: "blue"}}> the Terms and Conditions</span>
+  </span>)
+
   return (
     <Container>
-      <Button variant="primary">Here</Button>
-      <Form.Group controlId="checkbox">
-      <Form.Check type="checkbox" label="Agree to terms"/>
-      </Form.Group>
+      <Col md={12}>
+        <Form.Group className="m-3" controlId="checkbox">
+          <Form.Check onChange={() => setDissabled(!disabled)} type="checkbox" label={CheckBoxLabel}/>
+        </Form.Group>
+        <Button disabled={disabled} variant="primary">Confirm Order</Button>
+      </Col>
     </Container>
   )
 }
--- src/pages/summary/test/SummaryForm.test.tsx
+++ src/pages/summary/test/SummaryForm.test.tsx
@@ -4,12 +4,11 @@ import {render, screen, fireEvent} from '@testing-library/react';
 
 import SummaryForm from '../SummaryForm';
 
-test('Renders intital items', () => {
+test('Renders intital items in correct state and function', () => {
   render(<SummaryForm/>);
 
-  const button = screen.getByRole("button", {name: "Here"});
-
-  const checkBox = screen.getByRole("checkbox", {name:"Agree to terms"})
+  const button = screen.getByRole("button", {name: "Confirm Order"});
+  const checkBox = screen.getByRole("checkbox", {name:"I agree to the Terms and Conditions"})
   expect(checkBox).toBeInTheDocument();
   expect(button).toBeInTheDocument();
 
@@ -17,4 +16,19 @@ test('Renders intital items', () => {
 
 
 
+
+
 });
+
+test(" Checkbox enables on first click and disables on secound" , () =>{
+
+  render(<SummaryForm/>);
+  const button = screen.getByRole("button", {name: "Confirm Order"});
+  const checkBox = screen.getByRole("checkbox", {name:"I agree to the Terms and Conditions"})
+  fireEvent.click(checkBox)
+  expect(checkBox).toBeChecked()
+  expect(button).toBeEnabled()
+  fireEvent.click(checkBox)
+  expect(button).toBeDisabled()
+
+})
