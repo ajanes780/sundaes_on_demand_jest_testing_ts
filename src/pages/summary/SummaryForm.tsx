e42b48eefa9ce37ede92166f49334395704a523e
Aaron Janes <aaron.janes@gmail.com>
2021-08-25 14:52:27 -0600

    set up prettier and mock service worker

--- src/pages/summary/SummaryForm.tsx
+++ src/pages/summary/SummaryForm.tsx
@@ -1,40 +1,37 @@
-import React, {useState} from 'react';
-import {Container, Col, Button, Form, OverlayTrigger, Popover} from "react-bootstrap"
+import React, { useState } from 'react';
+import { Container, Col, Button, Form, OverlayTrigger, Popover } from 'react-bootstrap';
 
 function SummaryForm() {
-  const [disabled, setDissabled] = useState(true)
+  const [disabled, setDissabled] = useState(true);
   const [show, setShow] = useState(false);
 
-
-
   const mypopover = (
-    <Popover id="popover-basic">
-      <Popover.Body>
-        No ice creame will actually be delivered
-      </Popover.Body>
+    <Popover id='popover-basic'>
+      <Popover.Body>No ice creame will actually be deliveredl</Popover.Body>
     </Popover>
   );
 
-
   const CheckBoxLabel = (
     <span>
-    I agree to
-       <OverlayTrigger placement="right" overlay={mypopover}>
-          <span style={{color: "blue"}}>the Terms and Conditions</span>
-       </OverlayTrigger>
-    </span>)
+      I agree to
+      <OverlayTrigger placement='right' overlay={mypopover}>
+        <span style={{ color: 'blue' }}>the Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
 
-
-    return (
+  return (
     <Container>
       <Col md={12}>
-        <Form.Group className="m-3" controlId="checkbox">
-          <Form.Check onChange={() => setDissabled(!disabled)} type="checkbox" label={CheckBoxLabel}/>
+        <Form.Group className='m-3' controlId='checkbox'>
+          <Form.Check onChange={() => setDissabled(!disabled)} type='checkbox' label={CheckBoxLabel} />
         </Form.Group>
-        <Button disabled={disabled} variant="primary">Confirm Order</Button>
+        <Button disabled={disabled} variant='primary'>
+          Confirm Order
+        </Button>
       </Col>
     </Container>
-  )
+  );
 }
 
 export default SummaryForm;
